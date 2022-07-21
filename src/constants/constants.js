cadf45ce6500c0729b1ff4809de0dcbdc7b2c676
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2022-07-21 12:52:19 -0700

    done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,21 @@
 export const projects = [
   {
+    title: 'Delivery App',
+    description:"Demo of a delivery App, pay with paypal or when the driver arrives to your house, after that you will be redirected to the order's page where you can see the status of your order, the owner have a Admin page where he sees every order, and change the status of the order, I'd say that my main challenge was to put everything together I was having problems fetching the data, getting the user from one page to another, but mainly for lack of attention.",
+    image: '/images/delivery.png',
+    tags: ['React', 'nextjs', 'Mongodb', 'cloudinary'],
+    source: 'https://github.com/AlexanderGomes/delivery__app',
+    visit: 'https://delivery-app-chi.vercel.app/',
+    id: 0,
+  },
+  {
     title: 'Real Estate',
-    description: "A demo of a Real state application, the challenge with this application was knowing how to manipulate the API and how to build the search engine arount it !",
+    description: "A demo of a Real state application, the challenge with this application was knowing how to manipulate the API and how to build the search engine arount it.",
       image: '/images/realstate.png',
       tags: [ 'React', 'javascript'],
     source: 'https://github.com/AlexanderGomes/real__state',
     visit: 'https://real-state-d8uxhyjfr-alexandergomes.vercel.app/',
-    id: 0,
+    id: 1,
   },
   {
     title: 'E-Commerce',
@@ -15,26 +24,26 @@ export const projects = [
     tags: ['React', 'JavaScript', 'Stripe', 'Sanity'],
     source: 'https://github.com/AlexanderGomes/my__store',
     visit: 'https://my-store-seven.vercel.app/',
-    id: 1,
+    id: 2,
   },
   {
-    title: 'Mini social media',
+   title: 'Mini social media',
     description:"jwt authentication, post your thougths and see what other people have been thinking as well, you can see all the users that have been registered on this application, no one can see that your post belongs to you, so say what you always wanted to but couldn't ",
     image: '/images/social.png',
     tags: ['React', 'Nodejs', 'Mongodb', 'Express'],
     source: 'https://github.com/AlexanderGomes/login',
     visit: 'https://anonnymous.herokuapp.com/',
-    id: 2,
-  },
-  {
-    title: 'Delivery App',
-    description:"Demo of a delivery App, add to cart pay with paypal or when the driver arrives to your house, after that you will be redirected to the order page where you can see the status of your order, the owner had a Admin page where he sees every order, and change the status of the order, I'd say that my main challenge was to put everything together I was having problems fetching the data, getting the user from one page to another, but mainly for lack of attention.",
-    image: '/images/delivery.png',
-    tags: ['React', 'nextjs', 'Mongodb', 'cloudinary'],
-    source: 'https://github.com/AlexanderGomes/delivery__app',
-    visit: 'https://delivery-app-chi.vercel.app/',
     id: 3,
   },
+  {
+    title: 'MVP Social media',
+    description:"I was working on the functions that goes to every social media, the timeline, friend suggestions, posting pictures, sentences or both, liking and commenting posts, there was zero focus on the design of this application, I just wanted to build a backend and be able to malipulate it on the frontend.",
+    image: '/images/mvp.png',
+    tags: ['React', 'express', 'Mongodb', 'cloudinary', 'Node.js'],
+    source: 'https://github.com/AlexanderGomes/clone__social__media',
+    visit: 'https://a2g-social-media.herokuapp.com/',
+    id: 4,
+  }
 ];
 
 export const TimeLineData = [
